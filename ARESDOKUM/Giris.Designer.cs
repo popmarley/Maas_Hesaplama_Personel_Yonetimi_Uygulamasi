393f8488c6f06254c26d4f3c41baf97d683f3050
pop marley <hsynozguven@hotmail.com>
2023-09-21 16:17:57 +0300

    Guncelleme(UI guncellemeleri)

--- ARESDOKUM/Giris.Designer.cs
+++ ARESDOKUM/Giris.Designer.cs
@@ -28,30 +28,32 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris));
 			this.txt_UserName = new System.Windows.Forms.TextBox();
 			this.txt_UserPassword = new System.Windows.Forms.TextBox();
 			this.btn_Giris = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// txt_UserName
 			// 
 			this.txt_UserName.Location = new System.Drawing.Point(216, 212);
-			this.txt_UserName.Multiline = true;
 			this.txt_UserName.Name = "txt_UserName";
 			this.txt_UserName.Size = new System.Drawing.Size(224, 22);
 			this.txt_UserName.TabIndex = 1;
 			// 
 			// txt_UserPassword
 			// 
-			this.txt_UserPassword.Location = new System.Drawing.Point(216, 335);
-			this.txt_UserPassword.Multiline = true;
+			this.txt_UserPassword.Location = new System.Drawing.Point(216, 299);
 			this.txt_UserPassword.Name = "txt_UserPassword";
 			this.txt_UserPassword.Size = new System.Drawing.Size(224, 22);
 			this.txt_UserPassword.TabIndex = 2;
 			// 
 			// btn_Giris
 			// 
-			this.btn_Giris.Location = new System.Drawing.Point(288, 428);
+			this.btn_Giris.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btn_Giris.Location = new System.Drawing.Point(284, 379);
 			this.btn_Giris.Name = "btn_Giris";
 			this.btn_Giris.Size = new System.Drawing.Size(75, 23);
 			this.btn_Giris.TabIndex = 3;
@@ -59,15 +61,41 @@
 			this.btn_Giris.UseVisualStyleBackColor = true;
 			this.btn_Giris.Click += new System.EventHandler(this.btn_Giris_Click);
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.Location = new System.Drawing.Point(119, 215);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(96, 16);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Kullanıcı Adı:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(161, 301);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 16);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Şifre:";
+			// 
 			// Giris
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(614, 473);
+			this.BackgroundImage = global::ARESDOKUM.Properties.Resources.giris;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(653, 528);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.btn_Giris);
 			this.Controls.Add(this.txt_UserPassword);
 			this.Controls.Add(this.txt_UserName);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Giris";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Giris";
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -79,5 +107,7 @@
 		private System.Windows.Forms.TextBox txt_UserName;
 		private System.Windows.Forms.TextBox txt_UserPassword;
 		private System.Windows.Forms.Button btn_Giris;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
 	}
 }
