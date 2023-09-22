dab4acf1df4f599d023975c25dd5101e930740b4
Metin Yeni <metinyeni33@gmail.com>
2023-09-23 01:16:09 +0300

    Calculate Salary method created

--- ARESDOKUM/CalculateSalaryForm.Designer.cs
+++ ARESDOKUM/CalculateSalaryForm.Designer.cs
@@ -33,6 +33,13 @@
             this.btn_Exit = new System.Windows.Forms.Button();
             this.cb_EmployeeList = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dt_StartDate = new System.Windows.Forms.DateTimePicker();
+            this.dt_EndDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_CalculateSalary = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_Salary = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_Main
@@ -69,26 +76,103 @@
             // 
             this.cb_EmployeeList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.cb_EmployeeList.FormattingEnabled = true;
-            this.cb_EmployeeList.Location = new System.Drawing.Point(98, 173);
+            this.cb_EmployeeList.Location = new System.Drawing.Point(151, 81);
             this.cb_EmployeeList.Name = "cb_EmployeeList";
-            this.cb_EmployeeList.Size = new System.Drawing.Size(179, 24);
+            this.cb_EmployeeList.Size = new System.Drawing.Size(200, 24);
             this.cb_EmployeeList.TabIndex = 21;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(51, 176);
+            this.label1.Location = new System.Drawing.Point(106, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 16);
             this.label1.TabIndex = 22;
             this.label1.Text = "İşçi";
+            // 
+            // dt_StartDate
+            // 
+            this.dt_StartDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dt_StartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dt_StartDate.Location = new System.Drawing.Point(151, 124);
+            this.dt_StartDate.Name = "dt_StartDate";
+            this.dt_StartDate.Size = new System.Drawing.Size(200, 22);
+            this.dt_StartDate.TabIndex = 23;
+            // 
+            // dt_EndDate
+            // 
+            this.dt_EndDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dt_EndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dt_EndDate.Location = new System.Drawing.Point(151, 161);
+            this.dt_EndDate.Name = "dt_EndDate";
+            this.dt_EndDate.Size = new System.Drawing.Size(200, 22);
+            this.dt_EndDate.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Başlangıç Tarihi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(64, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Bitiş Tarihi";
+            // 
+            // btn_CalculateSalary
+            // 
+            this.btn_CalculateSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_CalculateSalary.Location = new System.Drawing.Point(32, 196);
+            this.btn_CalculateSalary.Name = "btn_CalculateSalary";
+            this.btn_CalculateSalary.Size = new System.Drawing.Size(319, 51);
+            this.btn_CalculateSalary.TabIndex = 25;
+            this.btn_CalculateSalary.Text = "Maaş Hesapla";
+            this.btn_CalculateSalary.UseVisualStyleBackColor = true;
+            this.btn_CalculateSalary.Click += new System.EventHandler(this.btn_CalculateSalary_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(497, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 20);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Ödenecek Tutar";
+            // 
+            // lbl_Salary
+            // 
+            this.lbl_Salary.AutoSize = true;
+            this.lbl_Salary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Salary.ForeColor = System.Drawing.Color.Green;
+            this.lbl_Salary.Location = new System.Drawing.Point(639, 84);
+            this.lbl_Salary.Name = "lbl_Salary";
+            this.lbl_Salary.Size = new System.Drawing.Size(39, 20);
+            this.lbl_Salary.TabIndex = 27;
+            this.lbl_Salary.Text = "000";
             // 
             // CalculateSalaryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1146, 598);
+            this.Controls.Add(this.lbl_Salary);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_CalculateSalary);
+            this.Controls.Add(this.dt_EndDate);
+            this.Controls.Add(this.dt_StartDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cb_EmployeeList);
             this.Controls.Add(this.btn_Main);
@@ -107,5 +191,12 @@
         private System.Windows.Forms.Button btn_Exit;
         private System.Windows.Forms.ComboBox cb_EmployeeList;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dt_StartDate;
+        private System.Windows.Forms.DateTimePicker dt_EndDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_CalculateSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_Salary;
     }
 }
--- ARESDOKUM/CalculateSalaryForm.cs
+++ ARESDOKUM/CalculateSalaryForm.cs
@@ -1,4 +1,5 @@
 ﻿using ARESDOKUM.Context;
+using ARESDOKUM.Entity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -44,5 +45,48 @@ namespace ARESDOKUM
         {
             LoadEmployeeList();
         }
+
+        private void btn_CalculateSalary_Click(object sender, EventArgs e)
+        {
+            // Seçilen çalışanı ve tarih aralığını alın
+            Employee selectedEmployee = (Employee)cb_EmployeeList.SelectedItem;
+            DateTime startDate = dt_StartDate.Value.Date;
+            DateTime endDate = dt_EndDate.Value;
+
+            using (var context = new MyDbContext()) // MyDbContext sınıfınıza uygun context adınızı kullanmalısınız
+            {
+                // Seçilen çalışanın tüm vardiyalarını belirtilen tarih aralığında alın
+                var shifts = context.Shifts
+                    .Where(s => s.EmployeeId == selectedEmployee.EmployeeId && s.Date >= startDate && s.Date <= endDate)
+                    .ToList();
+
+                // Toplam çalışma saatlerini hesaplayın
+                decimal totalHoursWorked = shifts.Sum(s => s.HoursWorked);
+
+                // Saatlik ücreti başlangıçta saatlik ücret olarak alın
+                decimal hourlyRate = selectedEmployee.BaseHourlyRate;
+
+                foreach (var shift in shifts)
+                {
+                    if (shift.Date.DayOfWeek == DayOfWeek.Saturday)
+                    {
+                        hourlyRate = selectedEmployee.BaseHourlyRate * 1.5m; // Cumartesi günleri saatlik ücreti 1.5 ile çarpın
+                    }
+                    else if (shift.Date.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        hourlyRate = selectedEmployee.BaseHourlyRate * 2m; // Pazar günleri saatlik ücreti 2 ile çarpın
+                    }
+                    // Diğer günlerde saatlik ücreti değişmeden devam edecek.
+                }
+
+                // Maaşı hesaplayın (saatlik ücret * toplam çalışma saatleri)
+                decimal salary = hourlyRate * totalHoursWorked;
+
+                // Sonucu label'e yazdırın
+                lbl_Salary.Text = $"{salary:C}";
+            }
+        }
+
+
     }
 }
